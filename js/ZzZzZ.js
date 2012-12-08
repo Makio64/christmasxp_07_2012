d37cf846d29411a95af5ea46db7822ff720bcabf
Makio64 <davronai@gmail.com>
2012-12-08 05:21:16 +0000

    scene1

--- js/ZzZzZ.js
+++ js/ZzZzZ.js
@@ -9,39 +9,58 @@ var ZzZzZ = function (container)
 	this.pass = 0;
 	this.texts = [];
 	this.opacitys = [];
+	this.state = 0;
 
 	var maxLetters = 5;
 
 	for(var i = 0; i < maxLetters; i++) {
 		var text = $("<img src='img/z.png' />");
 		text.css("top", (i*50)+"px");
+		text.css("opacity", 0);
 		text.css("left", String(Math.cos(PI * .7 * i)*30)+"px");
 		container.append(text);
 		this.texts.push(text);
-		this.opacitys.push(1);
+		this.opacitys.push(0);
+	}
+
+	this.close = function(){
+		this.state = 1;
+		//for (var i = this.texts.length - 1; i >= 0; i--) {
+		//	TweenLite.to(this.texts[i],.5+Math.random()*.5,{css:{scaleX:.8,scaleY:.8, opacity:0}});
+		//}
 	}
 
 	this.update = function(dt) {
 		this.t += dt/Math.PI/30;
-		var text;
-		for (var i = this.texts.length - 1; i >= 0; i--) {
-			text = this.texts[i];
-			opacity = this.opacitys[i];
-			text.css("left", String(Math.cos((this.t/6) * .7+Math.PI/2* i  )*40)+"px");
-			text.css("top", (text.position().top-dt/30)+"px");
-			if(text.position().top<0) {
-				this.opacitys[i] -= 0.05;
+		
+		if(this.state>=0){
+			var text;
+			for (var i = this.texts.length - 1; i >= 0; i--) {
+				text = this.texts[i];
 				opacity = this.opacitys[i];
-				text.css("opacity", this.opacitys[i]);
-				if(this.opacitys[i]<=0) {
-					this.opacitys[i] = 0;
-					text.css("top","250px");
+				if(this.state == 1){
+					this.opacitys[i] -= 0.05;
+					text.css("opacity", this.opacitys[i]);
 				}
-			} else if(this.opacitys[i] < 1) {
-				this.opacitys[i] += 0.02;
-				text.css("opacity", this.opacitys[i]);
-			};
-		};	
+				text.css("left", String(Math.cos((this.t/6) * .7+Math.PI/2* i  )*40)+"px");
+				text.css("top", (text.position().top-dt/30)+"px");
+				if(text.position().top<0) {
+					if(state == 1){
+						text.css("display","none");	
+					}
+					this.opacitys[i] -= 0.05;
+					opacity = this.opacitys[i];
+					text.css("opacity", this.opacitys[i]);
+					if(this.opacitys[i]<=0) {
+						this.opacitys[i] = 0;
+						text.css("top","250px");
+					}
+				} else if(this.opacitys[i] < 1 && this.state == 0) {
+					this.opacitys[i] += 0.02;
+					text.css("opacity", this.opacitys[i]);
+				};
+			};	
+		}
 	}
 }
 
